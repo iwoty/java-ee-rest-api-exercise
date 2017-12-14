9ff28907f98912384e49db62b6f0613b7d38ba06
Iwo <iwo.tyszkowski@gmail.com>
2017-12-14 10:41:15 +0100

    Add missing DAOs

--- src/main/java/com/codecool/DAO/EmployeeDAO.java
+++ src/main/java/com/codecool/DAO/EmployeeDAO.java
@@ -1,5 +1,89 @@
 package com.codecool.DAO;
 
+import com.codecool.models.Employee;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeDAO extends AbstractDAO {
 
+    private Connection connection;
+
+    public EmployeeDAO(Connection connectionToDB) {
+        connection = connectionToDB;
+    }
+
+    public List<Employee> readlAll() throws SQLException {
+        List<Employee> employeeList = new ArrayList<>();
+
+        String query = "SELECT * FROM employees;";
+        PreparedStatement statement = connection.prepareStatement(query);
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            Employee employee = this.createEmployeeFromResultSet(resultSet);
+            employeeList.add(employee);
+        }
+        return employeeList;
+    }
+
+    public Employee getByID(String ID) throws SQLException {
+        ResultSet rs = null;
+        String query = "SELECT * FROM employees WHERE id = ?;";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1, Integer.parseInt(ID));
+        rs = statement.executeQuery();
+
+        if (rs.next()) {
+            return this.createEmployeeFromResultSet(rs);
+        }
+        return null;
+    }
+
+    public void create(Employee employee) throws SQLException {
+        String query = "INSERT INTO employee (first_name, last_name, shop_id) VALUES (?, ?, ?);";
+        PreparedStatement statement = connection.prepareStatement(query);
+        setPreparedCreateStatement(statement, employee);
+        statement.executeUpdate();
+    }
+
+    private void setPreparedCreateStatement(PreparedStatement statement, Employee employee) throws SQLException {
+        statement.setString(1, employee.getFirstName());
+        statement.setString(1, employee.getLastName());
+        statement.setInt(2, employee.getShopID());
+    }
+
+    public void update(Employee employee) throws SQLException {
+        String query = "UPDATE employees SET first_name = ?, last_name = ?, shop_id = ? WHERE id = ?;";
+        PreparedStatement statement = connection.prepareStatement(query);
+        setPreparedUpdateStatement(statement, employee);
+        statement.executeUpdate();
+    }
+
+    private void setPreparedUpdateStatement(PreparedStatement statement, Employee employee) throws SQLException {
+        statement.setString(1, employee.getFirstName());
+        statement.setString(2, employee.getLastName());
+        statement.setInt(3, employee.getShopID());
+        statement.setInt(4, employee.getID());
+    }
+
+    public void delete(Employee employee) throws SQLException {
+        String query = "DELETE FROM employees WHERE id = ?;";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, String.valueOf(employee.getID()));
+        statement.executeUpdate();
+    }
+
+    Employee createEmployeeFromResultSet(ResultSet resultSet) throws SQLException {
+        int ID = Integer.parseInt(resultSet.getString("id"));
+        String firstName = resultSet.getString("first_name");
+        String lastName = resultSet.getString("last_name");
+        int shopID = Integer.parseInt(resultSet.getString("shop_id"));
+
+        return new Employee(ID, firstName, lastName, shopID);
+    }
 }
--- src/main/java/com/codecool/DAO/ShopDAO.java
+++ src/main/java/com/codecool/DAO/ShopDAO.java
@@ -1,5 +1,84 @@
 package com.codecool.DAO;
 
+import com.codecool.models.Employee;
+import com.codecool.models.Shop;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShopDAO extends AbstractDAO {
 
+    private Connection connection;
+
+    public ShopDAO(Connection connectionToDB) {
+        connection = connectionToDB;
+    }
+
+    public List<Shop> readlAll() throws SQLException {
+        List<Shop> shopList = new ArrayList<>();
+
+        String query = "SELECT * FROM shops;";
+        PreparedStatement statement = connection.prepareStatement(query);
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            Shop shop = this.createShopFromResultSet(resultSet);
+            shopList.add(shop);
+        }
+        return shopList;
+    }
+
+    public Shop getByID(String ID) throws SQLException {
+        ResultSet rs = null;
+        String query = "SELECT * FROM shops WHERE id = ?;";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1, Integer.parseInt(ID));
+        rs = statement.executeQuery();
+
+        if (rs.next()) {
+            return this.createShopFromResultSet(rs);
+        }
+        return null;
+    }
+
+    public void create(Shop shop) throws SQLException {
+        String query = "INSERT INTO shops (location) VALUES (?);";
+        PreparedStatement statement = connection.prepareStatement(query);
+        setPreparedCreateStatement(statement, shop);
+        statement.executeUpdate();
+    }
+
+    private void setPreparedCreateStatement(PreparedStatement statement, Shop shop) throws SQLException {
+        statement.setString(1, shop.getLocation());
+    }
+
+    public void update(Shop shop) throws SQLException {
+        String query = "UPDATE shops SET location = ? WHERE id = ?;";
+        PreparedStatement statement = connection.prepareStatement(query);
+        setPreparedUpdateStatement(statement, shop);
+        statement.executeUpdate();
+    }
+
+    private void setPreparedUpdateStatement(PreparedStatement statement, Shop shop) throws SQLException {
+        statement.setString(1, shop.getLocation());
+        statement.setInt(2, shop.getID());
+    }
+
+    public void delete(Shop shop) throws SQLException {
+        String query = "DELETE FROM shops WHERE id = ?;";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, String.valueOf(shop.getID()));
+        statement.executeUpdate();
+    }
+
+    Shop createShopFromResultSet(ResultSet resultSet) throws SQLException {
+        int ID = Integer.parseInt(resultSet.getString("id"));
+        String location = resultSet.getString("location");
+
+        return new Shop(ID, location);
+    }
 }
